0581e5a48e9114a3abc4e12fc6670e84079be726
XinyangNick <xinyang.gao@mail.utoronto.ca>
2024-10-16 20:23:57 -0700

    Add the Stage 2 transition price criteria

--- trading_function.py
+++ trading_function.py
@@ -5,16 +5,27 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
-def MA(history:yf.Ticker.history, N:int):
+def MA(history:pd.DataFrame, N:int):
     """
     MA = sum(Pn)/n, where Pn is Clsoed price
     """
     history['MA'+str(N)] = history['Close'].rolling(N).sum() / N
     
 
+def TransStage2_price_Criteria(history:pd.DataFrame, n=0.8):
+    """
+    52-week low
+    """
+    low52week = history['Close'].min()
+    last_price = history['Close'].iloc[-1]
+
+    return last_price >= low52week * (1+n)
+
+
+
 
 #Plot
-def Kline_Plot(ticker:yf.Ticker, data:pd.DataFrame):
+def Kline_Plot(Ticker:yf.Ticker, data:pd.DataFrame):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
                     vertical_spacing=0.03, subplot_titles=(f'Candlestick Chart for {ticker}', 'Volume'), 
                     row_width=[0.2, 0.7])
@@ -31,10 +42,12 @@ def Kline_Plot(ticker:yf.Ticker, data:pd.DataFrame):
     fig.add_trace(go.Bar(x=data.index, y=data['Volume'], showlegend=False, name='Volume'), row=2, col=1)
 
     # Update layout
-    fig.update_layout(title=f'{ticker} Candlestick Chart with Volume',
+    fig.update_layout(title=f'{Ticker} Candlestick Chart with Volume',
                     xaxis_title='Date',
                     yaxis_title='Price (USD)',
                     xaxis_rangeslider_visible=False)
 
     # Show the plot
     fig.show()
+
+

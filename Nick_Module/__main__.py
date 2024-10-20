6eb6e326d014680e23b199ddd4f5941fbcc59e0c
XinyangNick <xinyang.gao@mail.utoronto.ca>
2024-10-20 00:21:34 -0700

    Fix the wrong part in clean_data

--- Nick_Module/__main__.py
+++ Nick_Module/__main__.py
@@ -2,7 +2,7 @@ import graphing_function as gf
 import selection_function as sf
 import clean_data as cd
 
-Ticker = 'TXRH'
+Ticker = 'IRMD'
 print(sf.Stage2_Confirmed_Criteria(Ticker))
 gf.graph_move_together(Ticker, time='1y')
 cd.generate_fundamental(Ticker)

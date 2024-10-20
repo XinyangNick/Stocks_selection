b1124d1b8aa58deb2e348b2d84df9b938b086dd1
XinyangNick <xinyang.gao@mail.utoronto.ca>
2024-10-19 23:46:29 -0700

    clean the folder

--- Nick_Module/main.py
+++ Nick_Module/main.py
@@ -2,7 +2,7 @@ import graphing_function as gf
 import selection_function as sf
 import clean_data as cd
 
-Ticker = 'IRMD'
+Ticker = 'TXRH'
 print(sf.Stage2_Confirmed_Criteria(Ticker))
 gf.graph_move_together(Ticker, time='1y')
 cd.generate_fundamental(Ticker)
